21a86c86e7a1e29afd32ad8e34328feba0f3f64f
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-16 11:14:43 +0200

    Create StaffModel.cs

---
+++ Models/StaffModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InnovexBackend.Models
+{
+    [Table("Staff")]
+    public class StaffModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string username { get; set; } = string.Empty;
+
+        [Required]
+        public string password { get; set; } = string.Empty;
+
+        [Required]
+        public string fullname { get; set; } = string.Empty;
+
+        [Required]
+        public string email { get; set; } = string.Empty;
+
+        [Required]
+        public string roleTitle { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+    }
+}
