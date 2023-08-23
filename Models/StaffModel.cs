be3c53269566679af3ba161968e108603739112b
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-08-23 08:43:27 +0200

    Auth Done

--- Models/StaffModel.cs
+++ Models/StaffModel.cs
@@ -10,22 +10,18 @@ namespace InnovexBackend.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
         public string Username { get; set; } = string.Empty;
 
         [Required]
         public string Password { get; set; } = string.Empty;
 
-        [Required]
         public string Fullname { get; set; } = string.Empty;
 
         [Required]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
         public string RoleTitle { get; set; } = "User";
 
-        [Required]
         public bool IsActive { get; set; } = true;
 
     }
