a87003f139b8615422518c1bfb924aaf4e0d2c53
epicYellow <105212574+epicYellow@users.noreply.github.com>
2023-09-05 23:27:24 +0200

    Updated account model
    Add total frees transactions left

--- Migrations/20230905211422_Free limit in account.Designer.cs
+++ Migrations/20230905211422_Free limit in account.Designer.cs
@@ -2,6 +2,7 @@
 using InnovexBackend;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InnovexBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230905211422_Free limit in account")]
+    partial class Freelimitinaccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230905211422_Free limit in account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnovexBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Freelimitinaccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Free_transactions_left",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Free_transactions_left",
+                table: "Accounts");
+        }
+    }
+}
